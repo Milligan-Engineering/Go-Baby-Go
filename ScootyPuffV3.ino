f68585feb7db238355e04fc55844e20a0e467e94
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2021-01-18 14:30:08 -0500

    PS4 Controller Pairing
    This implements the ability to pair with the PS4 controller and interpret the inputs from the controller. The inputs have not yet been associated with specific motor control functions, so this is for demonstration alone. Later I will clean up the demonstration functions and streamline the program as a whole, associating specific controller inputs with the perspective motor control functions.

--- ScootyPuffV3.ino
+++ ScootyPuffV3.ino
@@ -1,11 +1,36 @@
-//############################################################################//
-//                             SCOOTYPUFF v.3                                 //
+//#######################/#####################################################//
+//                             SCOOTYPUFF v.4                                 //
 //                       "Who's ready for safe fun?"                          //
 //############################################################################//
 
 
-// Arduino pin assignments
 
+// Include libraries for PS4 Bluetooth and USB Host Shield Support
+#include <PS4BT.h>
+#include <usbhub.h>
+
+// Satisfy the IDE, which needs to see the include statment in the ino too.
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#endif
+#include <SPI.h>
+
+USB Usb;
+//USBHub Hub1(&Usb); // Some dongles have a hub inside
+BTD Btd(&Usb); // Create the Bluetooth Dongle instance
+
+// You can create the instance of the PS4BT class in two ways
+// This will start an inquiry and then pair with the PS4 controller - you only have to do this once
+// You will need to hold down the PS and Share button at the same time, the PS4 controller will then start to blink rapidly indicating that it is in pairing mode
+PS4BT PS4(&Btd, PAIR);
+
+// After that you can simply create the instance like so and then press the PS button on the device
+//PS4BT PS4(&Btd);
+
+bool printAngle, printTouch;
+uint8_t oldL2Value, oldR2Value;
+
+// Arduino pin assignments
 #define SERIAL_OUT           11 //assigned to pin 11
 #define SERIAL_IN            10
 
@@ -16,7 +41,7 @@
 #define BTN_RIGHT             4
 
 #define POT_GOVERNOR         A0
-#define POT_ACCELERATION     A1 //Milligan Engineering: Ensure this is installed or set to 1
+#define POT_ACCELERATION     A1 
 
 #define POT_TRIM             A2
 
@@ -30,7 +55,7 @@
 // The values in this section only can be modified to alter the behavior of the
 // individual ride-on toy.
 
-#define USE_ACCELERATION_POT  true //If there is no installed acceleration potentiometer, set to 'false'
+#define USE_ACCELERATION_POT  true  //If there is no installed acceleration potentiometer, set to 'false'
 
 
 
@@ -156,7 +181,7 @@ void setup(){
   pinMode(RELAY_FUN1,OUTPUT);
   pinMode(RELAY_FUN2,OUTPUT);
 
-  // initialize global variables
+// initialize global variables
   estopped = false;
 // initializing start-up/default values
   left_motor.name = "L";
@@ -179,18 +204,135 @@ void setup(){
   Serial.begin(BAUD_RATE);
   soft_serial.begin(BAUD_RATE);
 
+// Controller connection setup
+#if !defined(__MIPSEL__)
+  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+#endif
+  if (Usb.Init() == -1) {
+    Serial.print(F("\r\nOSC did not start"));
+    while (1); // Halt
+  }
+  Serial.print(F("\r\nPS4 Bluetooth Library Started"));
+
 }
 
 //##############################################################################
 
 void loop(){
 
+//##############################################################################
+//PS4 DATA, WILL BE ALTERING
+Usb.Task();
+
+  if (PS4.connected()) {
+    if (PS4.getAnalogHat(LeftHatX) > 137 || PS4.getAnalogHat(LeftHatX) < 117 || PS4.getAnalogHat(LeftHatY) > 137 || PS4.getAnalogHat(LeftHatY) < 117 || PS4.getAnalogHat(RightHatX) > 137 || PS4.getAnalogHat(RightHatX) < 117 || PS4.getAnalogHat(RightHatY) > 137 || PS4.getAnalogHat(RightHatY) < 117) {
+      Serial.print(F("\r\nLeftHatX: "));
+      Serial.print(PS4.getAnalogHat(LeftHatX));
+      Serial.print(F("\tLeftHatY: "));
+      Serial.print(PS4.getAnalogHat(LeftHatY));
+      Serial.print(F("\tRightHatX: "));
+      Serial.print(PS4.getAnalogHat(RightHatX));
+      Serial.print(F("\tRightHatY: "));
+      Serial.print(PS4.getAnalogHat(RightHatY));
+    }
+
+    if (PS4.getAnalogButton(L2) || PS4.getAnalogButton(R2)) { // These are the only analog buttons on the PS4 controller
+      Serial.print(F("\r\nL2: "));
+      Serial.print(PS4.getAnalogButton(L2));
+      Serial.print(F("\tR2: "));
+      Serial.print(PS4.getAnalogButton(R2));
+    }
+    if (PS4.getAnalogButton(L2) != oldL2Value || PS4.getAnalogButton(R2) != oldR2Value) // Only write value if it's different
+      PS4.setRumbleOn(PS4.getAnalogButton(L2), PS4.getAnalogButton(R2));
+    oldL2Value = PS4.getAnalogButton(L2);
+    oldR2Value = PS4.getAnalogButton(R2);
+
+    if (PS4.getButtonClick(PS)) {
+      Serial.print(F("\r\nPS"));
+      PS4.disconnect();
+    }
+    else {
+      if (PS4.getButtonClick(TRIANGLE)) {
+        Serial.print(F("\r\nTriangle"));
+        PS4.setRumbleOn(RumbleLow);
+      }
+      if (PS4.getButtonClick(CIRCLE)) {
+        Serial.print(F("\r\nCircle"));
+        PS4.setRumbleOn(RumbleHigh);
+      }
+      if (PS4.getButtonClick(CROSS)) {
+        Serial.print(F("\r\nCross"));
+        PS4.setLedFlash(10, 10); // Set it to blink rapidly
+      }
+      if (PS4.getButtonClick(SQUARE)) {
+        Serial.print(F("\r\nSquare"));
+        PS4.setLedFlash(0, 0); // Turn off blinking
+      }
+
+      if (PS4.getButtonClick(UP)) {
+        Serial.print(F("\r\nUp"));
+        PS4.setLed(Red);
+      } if (PS4.getButtonClick(RIGHT)) {
+        Serial.print(F("\r\nRight"));
+        PS4.setLed(Blue);
+      } if (PS4.getButtonClick(DOWN)) {
+        Serial.print(F("\r\nDown"));
+        PS4.setLed(Yellow);
+      } if (PS4.getButtonClick(LEFT)) {
+        Serial.print(F("\r\nLeft"));
+        PS4.setLed(Green);
+      }
+
+      if (PS4.getButtonClick(L1))
+        Serial.print(F("\r\nL1"));
+      if (PS4.getButtonClick(L3))
+        Serial.print(F("\r\nL3"));
+      if (PS4.getButtonClick(R1))
+        Serial.print(F("\r\nR1"));
+      if (PS4.getButtonClick(R3))
+        Serial.print(F("\r\nR3"));
+
+      if (PS4.getButtonClick(SHARE))
+        Serial.print(F("\r\nShare"));
+      if (PS4.getButtonClick(OPTIONS)) {
+        Serial.print(F("\r\nOptions"));
+        printAngle = !printAngle;
+      }
+      if (PS4.getButtonClick(TOUCHPAD)) {
+        Serial.print(F("\r\nTouchpad"));
+        printTouch = !printTouch;
+      }
+
+      if (printAngle) { // Print angle calculated using the accelerometer only
+        Serial.print(F("\r\nPitch: "));
+        Serial.print(PS4.getAngle(Pitch));
+        Serial.print(F("\tRoll: "));
+        Serial.print(PS4.getAngle(Roll));
+      }
+
+      if (printTouch) { // Print the x, y coordinates of the touchpad
+        if (PS4.isTouching(0) || PS4.isTouching(1)) // Print newline and carriage return if any of the fingers are touching the touchpad
+          Serial.print(F("\r\n"));
+        for (uint8_t i = 0; i < 2; i++) { // The touchpad track two fingers
+          if (PS4.isTouching(i)) { // Print the position of the finger if it is touching the touchpad
+            Serial.print(F("X")); Serial.print(i + 1); Serial.print(F(": "));
+            Serial.print(PS4.getX(i));
+            Serial.print(F("\tY")); Serial.print(i + 1); Serial.print(F(": "));
+            Serial.print(PS4.getY(i));
+            Serial.print(F("\t"));
+          }
+        }
+      }
+    }
+  }
+
+ //############################################################################################### 
   digitalWrite(LED_HEARTBEAT,HIGH);
 
   if (estopped != true){
+    update_acceleration();
     update_governor();
     update_trim();
-    update_acceleration();
     read_buttons();
   } else {
     pulse_estop_led();
@@ -201,7 +343,6 @@ void loop(){
 
   digitalWrite(LED_HEARTBEAT,LOW);
   delay(LOOP_DELAY);
-
 }
 
 //##############################################################################
@@ -341,7 +482,7 @@ void update_trim(){
 void update_acceleration(){
 
   if (USE_ACCELERATION_POT){
-    acceleration = map(analogRead(POT_ACCELERATION), 0, 1023, 1, (max_speed/4) );
+    acceleration = map(analogRead(POT_ACCELERATION), 0, 1023, 1, (max_speed)/4 );
     deceleration = acceleration * 2;
     Serial.print( " acceleration: ");
     Serial.println(acceleration);
@@ -353,6 +494,7 @@ void update_acceleration(){
   acceleration = 1;
   deceleration = acceleration * 2;
   }
+  
 
 }
 
@@ -406,7 +548,7 @@ void govern_motor(Motor *motor){
 
 //##############################################################################
 
-void do_estop(){
+void do_estop(){  //Emergency Stop
 
   estopped = true;
   left_motor.speed = 0;
@@ -464,6 +606,7 @@ void tandem_accelerate(Direction direction){
 void do_forward(){
 
   tandem_accelerate(FORWARD);
+  Serial.println("DO FORWARD");
 
 }
 
@@ -472,6 +615,7 @@ void do_forward(){
 void do_reverse(){
 
   tandem_accelerate(REVERSE);
+  Serial.println("DO REVERSE");
 
 }
 
@@ -481,6 +625,7 @@ void do_left(){
 
   accelerate(&left_motor,REVERSE,acceleration);
   accelerate(&right_motor,FORWARD,acceleration);
+  Serial.println("DO LEFT");
 
 }
 
@@ -490,6 +635,16 @@ void do_right(){
 
   accelerate(&left_motor,FORWARD,acceleration);
   accelerate(&right_motor,REVERSE,acceleration);
+  Serial.println("DO RIGHT");
+
+}
+
+//##############################################################################
+
+void do_forward_vector_right(){
+
+  accelerate(&left_motor,FORWARD,acceleration);
+  accelerate(&right_motor,FORWARD,acceleration*(1/2));
 
 }
 
